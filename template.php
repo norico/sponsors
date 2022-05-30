d50fcd232e253dbf6c3da37625bc5dbb1676f77e
norico <norico.legrand@gmail.com>
2022-05-30 09:43:58 +0200

    Move placeholder file in media dir

--- template.php
+++ template.php
@@ -2,7 +2,7 @@
 if ( isset($instance) )
 {
     $title  = $post->post_title;
-    $image  = has_post_thumbnail() ? get_the_post_thumbnail($post->ID, 'medium', array('alt'=> $post->post_title, 'title'=> $post->post_title)) : '<img src="'.plugin_dir_url(__FILE__).'placeholder.jpg" width="150" height="150" alt="'.$post->post_title.'">';
+    $image  = has_post_thumbnail() ? get_the_post_thumbnail($post->ID, 'medium', array('alt'=> $post->post_title, 'title'=> $post->post_title)) : '<img src="'.plugin_dir_url(__FILE__).'/media/placeholder.jpg" width="150" height="150" alt="'.$post->post_title.'">';
     $link   = get_the_permalink($post->ID);
     $count  = get_post_meta( $post->ID, $this->meta_count, true );
     $target = get_post_meta( $post->ID, $this->target, true );
